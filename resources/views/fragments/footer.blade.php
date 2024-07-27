7249e6adc3a61c5105ca8c81fde230b13a5cf410
Hans Rondón <hansrondon1998@gmail.com>
2024-07-27 09:47:07 -0500

    Refactor - se ajustan textos del footer

--- resources/views/fragments/footer.blade.php
+++ resources/views/fragments/footer.blade.php
@@ -1,6 +1,6 @@
-<footer class="footer mt-auto py-3 bg-light">
+<footer class="footer mt-auto py-3 bg-light text-center">
     <div class="container">
-        <a target="_blank" href="https://icons8.com/icon/R3SjD8mgrmca/amazon">Amazonas</a> icono de <a target="_blank" href="https://icons8.com">Icons8</a>
-        <a href="https://www.flaticon.es/iconos-gratis/info" title="info iconos">Info iconos creados por Roundicons - Flaticon</a>
+        <p><a target="_blank" href="https://icons8.com/icon/R3SjD8mgrmca/amazon">Amazonas</a> icono de <a target="_blank" href="https://icons8.com">Icons8</a></p>
+        <p><a href="https://www.flaticon.es/iconos-gratis/info" title="info iconos">Info iconos creados por Roundicons - Flaticon</a></p>
     </div>
 </footer>
