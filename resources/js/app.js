f8026b701af30f598dae4d81e6c9d88eed5fe4e5
Hans Rondón <hansrondon1998@gmail.com>
2024-07-27 09:47:54 -0500

    Refactor - se mejora experiencia de usuario en el registro agregando indicador de carga

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,24 @@
-import './bootstrap';
+/**
+ * El siguiente script controla el envío de los datos
+ * por ajax al backend
+ */
 $('#registerForm').on('submit', function(event) {
     event.preventDefault();
 
+    // Indicador de carga
+    Swal.fire({
+        title: 'Cargando...',
+        text: 'Por favor espera mientras procesamos tu solicitud',
+        allowOutsideClick: false,
+        didOpen: () => {
+            Swal.showLoading();
+        }
+    });
+
+    // Se pasa la información del formulario a un formData()
     const formData = new FormData(this);
+
+    // Solicitud ajax
     $.ajax({
         url: '/register',
         method: 'POST',
@@ -10,6 +26,9 @@ $('#registerForm').on('submit', function(event) {
         processData: false,
         contentType: false,
         success: function(response) {
+            // Se cierra el indicador de carga
+            Swal.close();
+
             Swal.fire({
                 title: `Registro exitoso!`,
                 text: response.statusText,
@@ -19,6 +38,9 @@ $('#registerForm').on('submit', function(event) {
             })
         },
         error: function(error) {
+            // Se cierra el indicador de carga
+            Swal.close();
+
             let message
             if(error.status === 422) message = 'Las contraseñas no coinciden'
             else message = error.statusText
