362aa079020b79a2a5b60937b4773bab9d16b225
Hans Rondón <hansrondon1998@gmail.com>
2024-07-27 10:43:28 -0500

    Refactor - se agregan iconos y se ajusta diseño

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -20,16 +20,17 @@
             </div>
             <div class="form-group">
                 <label for="password">Contraseña</label>
-                <input type="password" class="form-control" id="password" name="password" placeholder="Debe tener al menos 6 caracteres" required minlength="6">
-                <small class="form-text text-muted">La contraseña debe tener al menos 6 caracteres.</small>
+                <input type="password" class="form-control" id="password" name="password" placeholder="Debe tener al menos 6 caracteres" required minlength="6" style="margin-bottom: 0px !important;">
+                <small class="form-text text-muted"><img src="{{ asset('assets/icono-info.png') }}" class="me-1" alt="Logo" width="15px">La contraseña debe tener al menos seis caracteres.</small>
             </div>
-            <div class="form-group">
+            <div class="form-group mt-2">
                 <label for="password_confirmation">Vuelve a escribir la contraseña</label>
                 <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" required minlength="6">
             </div>
-            <button type="submit" class="btn btn-primary">Crear tu cuenta de Amazon</button>
-            <p class="mt-3">Al crear una cuenta, aceptas las <a href="#">Condiciones de Uso</a> y el <a href="#">Aviso de Privacidad de Amazon</a>.</p>
-            <p><a href="#">¿Ya tienes una cuenta? Iniciar sesión</a></p>
+            <button type="submit">Crear tu cuenta de Amazon</button>
+            <p class="mt-3 policies">Al crear una cuenta, aceptas las <a href="#">Condiciones de Uso</a> y el <a href="#">Aviso de Privacidad</a> de Amazon.</p>
+            <hr>
+            <p>¿Ya tienes una cuenta? <a href="#" class="text-decoration-none">Iniciar sesión <img src="{{ asset('assets/arrow-right.png') }} " width="6px"></a></p>
         </form>
     </div>
 @endsection
